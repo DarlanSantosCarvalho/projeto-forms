0e4fb8ceec593269c8dc8fcc08a07abbf823ac34
Engenharia Clinica <engclinica@labcmi.org.br>
2023-05-18 12:02:43 -0300

    Projeto foi atualizado, consegui transformar as repostas em JSON e irei fazer com que possa ser lido num banco de dados

--- src/Components/Forms/forms.jsx
+++ src/Components/Forms/forms.jsx
@@ -1,43 +1,51 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
 import "./forms.css"
 
 function Forms() {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (e) => {
+        console.log(e)
+    }
+
     return (
-        <form>
-
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="main">
-
                 <div className="nome">
                     <label>Nome:</label>
-                    <input type="text" name="name" id="name" placeholder="Digite seu nome" />
+                    <input type="text" {...register('name')} id="name" placeholder="Digite seu nome" />
                     <label>Sobrenome:</label>
-                    <input type="text" name="surname" id="surname" placeholder="Digite seu sobrenome" />
+                    <input type="text" {...register('surname')} id="surname" placeholder="Digite seu sobrenome" />
                 </div>
                 <div className="setor">
-                    <label for="estado">Setor:</label>
-                    <select id="estado" name="estado">
-                        <option value="CC">Centro cirúrgico</option>
-                        <option value="UTIPA">UTI Pediatria A</option>
-                        <option value="UTIPB">UTI Pediatria B</option>
-                        <option value="UTIPC">UTI Pediatria C</option>
+                    <label for="setor">Setor:</label>
+                    <select id="setor" {...register('setor')}>
+                        <option value="NA">Escolher setor</option>
+                        <option value="Centro Cirúrgico">Centro cirúrgico</option>
+                        <option value="UTI Pediatria A">UTI Pediatria A</option>
+                        <option value="UTI Pediatria B">UTI Pediatria B</option>
+                        <option value="UTI Pediatria C">UTI Pediatria C</option>
                     </select>
                 </div>
                 <div className="tempo">
                     <label>Data:</label>
-                    <input type="date" name="date" id="date" />
+                    <input type="date" {...register('date')} id="date" />
                     <label>Horário de início:</label>
-                    <input type="time" name="time_start" id="time_start" />
+                    <input type="time"{...register('timeStart')} id="timeStart" />
                     <label>Horário de saída:</label>
-                    <input type="time" name="time_end" id="time_end" />
+                    <input type="time" {...register('timeEnd')} id="timeEnd" />
                 </div>
                 <div className="tecnico">
                     <label for="Técnico executor: ">Técnico executor:</label>
-                    <select id="tecnico" name="tecnico">
-                        <option value="Robson">Robson Silva</option>
-                        <option value="Marcele">Marcele Fonseca</option>
-                        <option value="Vitor">Vitor Torres</option>
-                        <option value="Monique">Monique Coutinho</option>
-                        <option value="Igor">Igor Giovani</option>
+                    <select id="tecnico" {...register('tecnico')}>
+                        <option value="NA">Escolher técnico</option>
+                        <option value="Robson Silva">Robson Silva</option>
+                        <option value="Marcele Fonseca">Marcele Fonseca</option>
+                        <option value="Vitor Torres">Vitor Torres</option>
+                        <option value="Monique Coutinho">Monique Coutinho</option>
+                        <option value="Igor Giovani">Igor Giovani</option>
                         <option value="EngClin">Engenharia Clínica</option>
                     </select>
                 </div>
@@ -49,39 +57,39 @@ function Forms() {
 
                 <div className="equipment-1">
                     <p>VERIFICAR INTEGRIDADE DA BRAÇADEIRA E EXTENSOR DE PNI</p>
-                    <input type="radio" name="conformity-1" id="conformity" />
+                    <input type="radio" {...register('eqp1conformity-1')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-1" id="inconformity" />
+                    <input type="radio" {...register('eqp1conformity-1')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR INTEGRIDADE DO CABO DE ECG</p>
-                    <input type="radio" name="conformity-2" id="conformity" />
+                    <input type="radio" {...register('eqp1conformity-2')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-2" id="nonconformity" />
+                    <input type="radio" {...register('eqp1conformity-2')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR INTEGRIDADE DO SENSOR DE OXIMETRIA E PRÉ CABO (SE EXISTIR) </p>
-                    <input type="radio" name="conformity-3" id="conformity" />
+                    <input type="radio" {...register('eqp1conformity-3')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-3" id="nonconformity" />
+                    <input type="radio" {...register('eqp1conformity-3')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR PRESENÇA DE PARÂMETRO DOS MÓDULOS EXTERNOS NA TELA</p>
-                    <input type="radio" name="conformity-4" id="conformity" />
+                    <input type="radio" {...register('eqp1conformity-4')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-4" id="nonconformity" />
+                    <input type="radio" {...register('eqp1conformity-4')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR FUNCIONAMENTO DE CABO PAM COM SIMULADOR</p>
-                    <input type="radio" name="conformity-5" id="conformity" />
+                    <input type="radio" {...register('eqp1conformity-5')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-5" id="nonconformity" />
+                    <input type="radio" {...register('eqp1conformity-5')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR SE OS MONITORES ESTÃO LIGADOS NA REDE ELÉTRICA</p>
-                    <input type="radio" name="conformity-6" id="conformity" />
+                    <input type="radio" {...register('eqp1conformity-6')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-6" id="nonconformity" />
+                    <input type="radio" {...register('eqp1conformity-6')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
                 </div>
 
@@ -90,43 +98,45 @@ function Forms() {
                     <h2>APARELHO DE ANESTESIA</h2>
 
                     <p>VERIFICAR INTEGRIDADE DA BRAÇADEIRA E EXTENSOR DE PNI</p>
-                    <input type="radio" name="conformity-1" id="conformity" />
+                    <input type="radio" {...register('eqp2conformity-1')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-1" id="inconformity" />
+                    <input type="radio" {...register('eqp2conformity-1')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR INTEGRIDADE DO CABO DE ECG</p>
-                    <input type="radio" name="conformity-2" id="conformity" />
+                    <input type="radio" {...register('eqp2conformity-2')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-2" id="nonconformity" />
+                    <input type="radio" {...register('eqp2conformity-2')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR INTEGRIDADE DO SENSOR DE OXIMETRIA E PRÉ CABO (SE EXISTIR) </p>
-                    <input type="radio" name="conformity-3" id="conformity" />
+                    <input type="radio" {...register('eqp2conformity-3')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-3" id="nonconformity" />
+                    <input type="radio" {...register('eqp2conformity-3')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR PRESENÇA DE PARÂMETRO DOS MÓDULOS EXTERNOS NA TELA</p>
-                    <input type="radio" name="conformity-4" id="conformity" />
+                    <input type="radio" {...register('eqp2conformity-4')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-4" id="nonconformity" />
+                    <input type="radio" {...register('eqp2conformity-4')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR FUNCIONAMENTO DE CABO PAM COM SIMULADOR</p>
-                    <input type="radio" name="conformity-5" id="conformity" />
+                    <input type="radio" {...register('eqp2conformity-5')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-5" id="nonconformity" />
+                    <input type="radio" {...register('eqp2conformity-5')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                     <p>VERIFICAR SE OS MONITORES ESTÃO LIGADOS NA REDE ELÉTRICA</p>
-                    <input type="radio" name="conformity-6" id="conformity" />
+                    <input type="radio" {...register('eqp2conformity-6')} value="Conforme" id="conformity" />
                     <label for="integridadePNI">Conforme</label>
-                    <input type="radio" name="conformity-6" id="nonconformity" />
+                    <input type="radio" {...register('eqp2conformity-6')} value="Inconforme" id="nonconformity" />
                     <label for="integridadePNI">Não conforme</label>
 
                 </div>
             </section>
+
+            <button type='submit'>Enviar</button>
 
         </form >
     )
